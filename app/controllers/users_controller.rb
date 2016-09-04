eddcfc96dcc39e4d5be7a737541e2918af7c1e10
urzp <urzp002@mail.ru>
2016-09-04 14:07:49 +0300

    add flash and complite action create user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,13 +3,14 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)    # Not the final implementation!
     if @user.save
-      # Handle a successful save.
+      redirect_to @user
     else
       render 'new'
     end
   end
 
   def show
+    flash[:success] = "Welcome to the Sample App!"
     @user = User.find(params[:id])
   end
 
