7076a2d12f564caca81c28eb6cfb3cc0c14b8528
urzp <urzp002@mail.ru>
2016-09-04 13:27:14 +0300

    add create ation and messages of errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,27 @@
 class UsersController < ApplicationController
 
+  def create
+    @user = User.new(user_params)    # Not the final implementation!
+    if @user.save
+      # Handle a successful save.
+    else
+      render 'new'
+    end
+  end
+
   def show
     @user = User.find(params[:id])
   end
 
   def new
     @user = User.new
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+                                 :password_confirmation)
   end
 
 end
