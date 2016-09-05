87cb931b77583227f419cb61f9f0dbb233bda77c
urzp <urzp002@mail.ru>
2016-09-05 11:24:21 +0300

    create edit function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,26 @@
 class UsersController < ApplicationController
 
+
+  def update
+    @user = User.find(params[:id])
+    if @user.authenticate(params[:user][:old_password]) && @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      flash[:danger] = 'Invalid old password' if !@user.authenticate(params[:user][:old_password])
+      render 'edit'
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
   def create
     @user = User.new(user_params)    # Not the final implementation!
     if @user.save
       flash[:success] = "Welcome to the Sample App!"
+      sign_in @user
       redirect_to @user
     else
       render 'new'
