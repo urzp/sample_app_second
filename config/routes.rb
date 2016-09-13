ea62ec8f624229b9ea8e0a83af8b69dbb6614125
urzp <urzp002@mail.ru>
2016-09-13 20:34:28 +0300

    Add view of statistics and buttom follow/unfolliw

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
 
+  resources	:relationships,	only:	[:create,	:destroy]
+
+  resources	:users	do
+    member	do
+        get	:following,	:followers
+    end
+  end
 
   resources :microposts, only: [:create, :destroy]
 
