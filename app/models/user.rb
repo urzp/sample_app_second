255c4a794719270baffc883797218306515a3328
urzp <urzp002@mail.ru>
2016-09-13 09:54:58 +0300

    revers relationship for user.followers

--- app/models/user.rb
+++ app/models/user.rb
@@ -54,4 +54,10 @@ class User < ActiveRecord::Base
   has_many	:relationships,	foreign_key:	"follower_id",	dependent:	:destroy
   has_many	:followed_users,	through:	:relationships,	source:	:followed
 
+  # revers relationship for user.followers
+  has_many	:reverse_relationships,	foreign_key:	"followed_id",
+                                    class_name:		"Relationship",
+                                    dependent:			:destroy
+  has_many	:followers,	through:	:reverse_relationships,	source:	:follower
+
 end
